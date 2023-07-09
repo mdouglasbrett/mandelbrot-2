aa90abd45ea8cd478db4872fdc393b23c0028e60
Michael Brett <mdouglasbrett@icloud.com>
2023-07-09 19:38:40 +0100

    Adds comment formatting
    🤷‍♀️

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use num::Complex;
 use std::str::FromStr;
 
-/// Try to determine if 'c' is in the Mandelbrot set, using at most 'limit' of
+/// Try to determine if `c` is in the Mandelbrot set, using at most `limit` of
 /// iterations it took to decide.
 ///
-/// If 'c' is not a member, return Some(i), where 'i' is the number of iterations
-/// it took for 'c' to leave the circle of radius 2 centered on the origin. If 'c'
+/// If `c` is not a member, return Some(i), where 'i' is the number of iterations
+/// it took for `c` to leave the circle of radius 2 centered on the origin. If `c`
 /// seems to be a member (more precisely, if we reached the iteration limit without
-/// being able to prove 'c' is a not a member), return None.
+/// being able to prove `c` is a not a member), return None.
 pub fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0., im: 0., };
     for i in 0..limit {
