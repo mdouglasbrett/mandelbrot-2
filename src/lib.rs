170237693eaec966368ddfe92dd41f55cd13fae1
Michael Brett <mdouglasbrett@icloud.com>
2023-07-16 23:12:00 +0100

    Adds concurrency via crossbeam

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,7 @@ fn test_parse_complex() {
 /// `pixel` is a (column, row) pair indicating a particular pixel in that image.
 /// The `upper_left` and `lower_right` parameters are points on the complex
 /// plane designating the area that our image covers.
-fn pixel_to_point(
+pub fn pixel_to_point(
     bounds: (usize, usize),
     pixel: (usize, usize),
     upper_left: Complex64,
