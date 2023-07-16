e9c9f7939fe072e54e32a01464cb5b13edc8d31a
Michael Brett <mdouglasbrett@icloud.com>
2023-07-16 16:04:53 +0100

    Adds pixel_to_point and assoc. test

--- src/lib.rs
+++ src/lib.rs
@@ -71,4 +71,42 @@ fn test_parse_complex() {
     assert_eq!(parse_complex(",-0.0625"), None);
 }
 
-fn pixel_to_point() {}
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plane.
+///
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column, row) pair indicating a particular pixel in that image.
+/// The `upper_left` and `lower_right` parameters are points on the complex
+/// plane designating the area that our image covers.
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex64,
+    lower_right: Complex64,
+) -> Complex64 {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+
+    Complex64 {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 200),
+            (25, 175),
+            Complex64 { re: -1.0, im: 1.0 },
+            Complex64 { re: 1.0, im: -1.0 }
+        ),
+        Complex64 {
+            re: -0.5,
+            im: -0.75
+        }
+    )
+}
