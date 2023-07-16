2ac8b1a2349b5a1117110fd50716d0bc971f52e6
Michael Brett <mdouglasbrett@icloud.com>
2023-07-16 14:37:51 +0100

    Adds complex number parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use num::Complex;
+use num::{complex::Complex64, Complex};
 use std::str::FromStr;
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit` of
@@ -9,7 +9,7 @@ use std::str::FromStr;
 /// seems to be a member (more precisely, if we reached the iteration limit without
 /// being able to prove `c` is a not a member), return None.
 pub fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-    let mut z = Complex { re: 0., im: 0., };
+    let mut z = Complex { re: 0., im: 0. };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
             return Some(i);
@@ -39,6 +39,15 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     }
 }
 
+/// Parse a pair of floating-point numbers separated by a comma as a complex
+/// number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        _ => None,
+    }
+}
+
 #[test]
 fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("", ','), None);
@@ -48,4 +57,16 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
     assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+    assert_eq!(parse_complex(",-0.0625"), None);
 }
